daf66111713a607c2e84147e7bfb5f11e192283e
Chris W. Burke <chris@chriswburke.com>
2016-02-16 15:25:05 -0500

    Replacing Gulp Tasks with NPM scripts

--- tasks/default.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var gulp = require( 'gulp' );
-
-gulp.task( 'default', ['build'] );
-gulp.task( 'test', [ 'clean', 'sass:lint', 'sass:dev', 'sass:prod' ]);
-gulp.task( 'build', [ 'clean', 'sass:prod', 'sassdoc' ]);
